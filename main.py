88c676dda5e38dd56ba31fa26b6bb6e6ada3bf41
kral4 <68253860+rootkral4@users.noreply.github.com>
2021-02-06 08:10:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -19,12 +19,27 @@ oynabuton      = "images/oynabuton.png"
 kabuletbuton   = "images/kabuletbuton.png"
 lanebutons     = "images/lanebutons.png"
 champsearchbox = "images/champsearchbox.png"
-veigarpicture  = "images/veigarpic.png"
+champpicture  = "images/veigarpic.png"
 hazirbuton     = "images/hazirbuton.png"
 profilebuton   = "images/profilebuton.png"
 levelup        = "images/levelup.png"
 
-
+def getconfig():
+    with open("config.cfg", "r") as f:
+        lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        if "#" not in line:
+            line = line.split(":")
+            if line[0] == "STOPAFTER":
+                STOPAFTER = line[1]
+            elif line[0] == "SELECTCHAMP":
+                SELECTCHAMP = line[1]
+            elif line[0] == "LEVELUP":
+                LEVELUP = line[1]
+            elif line[0] == "BUY":
+                BUY = line[1]
+    return STOPAFTER, SELECTCHAMP, LEVELUP, BUY  
 
 def searchgame():
     pos = imagesearch(oynabuton)
@@ -73,12 +88,12 @@ def acceptuntilfound():
         if inchampselect == True:
             break
 
-def selectchamp():
+def selectchamp(champ):
     pos = imagesearch(champsearchbox)
     pyautogui.click(pos[0] + 45, pos[1] + 5)
-    pyautogui.write("sylas")
+    pyautogui.write(champ)
     time.sleep(1)
-    pos2 = imagesearch(veigarpicture)
+    pos2 = imagesearch(champpicture)
     ahk.click(pos2[0] + 2, pos2[1] + 2)
     print("Champion Selected")
     pos3 = imagesearch(hazirbuton)
@@ -117,13 +132,12 @@ def lvlup(skill):
 
 def attack():
     skilllist = ["q", "w", "e"]
-    pos = ahk.pixel_search(0xc4451f,variation=5)
+    pos = ahk.pixel_search(0xc4451f,variation=3)
     if pos != None:
         ahk.mouse_move(pos[0], pos[1])
         ahk.key_press(random.choice(skilllist))
-        print("Skill Used")
 
-global killthreads
+
 killthreads = False
 
 def killthread():
@@ -134,8 +148,9 @@ def checkforlevelup():
     global killthreads
     while True:
         if killthreads == True:
+            print("levelup thread killed")
             break
-        pos = imagesearch(levelup, precision=100)
+        pos = imagesearch(levelup)
         if pos != -1:
             ahk.click(pos[0] + 5, pos[1] + 5)
             ahk.click(pos[0] + 5, pos[1] + 5)
@@ -147,27 +162,43 @@ def randomspell():
     global killthreads
     while True:
         if killthreads == True:
+            print("randomspell thread killed")
             break
-        time.sleep(160)
+        time.sleep(120)
         ahk.key_press(random.choice(spelllist))
-        print("Spell Used")
 
 def autoward():
     global killthreads
     while True:
         if killthreads == True:
+            print("autoward thread killed")
             break
         time.sleep(60)
         ahk.key_press("4")
         print("Ward Placed")
 
+def gamecounter():
+    with open("log.txt", "r") as f:
+        num = f.readline()
+        num = num.strip() 
+        num = int(num)
+        with open("log.txt", "w") as f:
+            num += 1
+            f.write(str(num))
+    return num
+
 def checkforendgame():
     global killthreads
+    conf = getconfig()
     while True:
         if killthreads == True:
+            print("checkforendgame thread killed")
             break
         win = ahk.find_window(title=b'League of Legends (TM) Client')
         if win == None:
+            num = gamecounter()
+            if num == int(conf[0]):
+                os.kill(os.getpid(), 1)      
             print("Match Ended")
             killthread()
             print("Threads Killed")
@@ -179,7 +210,14 @@ def checkforendgame():
             subprocess.Popen("\"C:\Riot Games\Riot Client\RiotClientServices.exe\" --launch-product=league_of_legends --launch-patchline=live")
             time.sleep(20)
             print("LOL started")
-            start()
+            print("waiting threads to be dead")
+            while True:
+                if threading.active_count() <= 2:
+                    start()
+                    break
+                else:
+                    time.sleep(5)
+                    print(threading.active_count())
 
 def startthreads():
     threading.Thread(target=checkforlevelup).start()
@@ -190,18 +228,19 @@ def startthreads():
 def start():
     win = ahk.find_window(title=b'League of Legends')
     win.activate()
+    conf = getconfig()
     searchgame()
     acceptuntilfound()
-    selectchamp()
+    selectchamp(conf[1])
     checkforgamebreak()
     while True:
         pos = imagesearch(levelup)
         if pos != -1:
             time.sleep(30)
-            playgame()
+            playgame(conf[2], conf[3])
             break
 
-def playgame():
+def playgame(LEVELUP, BUY):
     global killthreads
     killthreads = False
     time.sleep(2)
@@ -211,23 +250,28 @@ def playgame():
     else:
         win.activate()
         time.sleep(1)
-        lvlup("q")
+        lvlup(LEVELUP)
         ahk.key_press("p")
         ahk.click(479, 274)
-        ahk.send_input("yuzuk")
+        ahk.send_input(BUY)
         ahk.right_click(360, 377)
         time.sleep(0.5)
         ahk.key_press("Esc")
         startthreads()
         while True:
             if killthreads == True:
+                print("playgame thread killed")
+                f2up()
                 break
             f2down()
             ahk.right_click(953, 552)
             time.sleep(1)
             attack()
+            f2up()
+            
 
 atexit.register(killthread)
+
 while True:
     opts = """
     Options :
@@ -240,7 +284,8 @@ while True:
         start()
         break
     elif choice == "Start":
-        playgame()
+        conf = getconfig()
+        playgame(conf[2], conf[3])
         break
     else:
         print("No such option")
